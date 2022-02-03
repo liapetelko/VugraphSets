b3083c41c7aa6ad3303d9579a2f1879d6a86e7b1
liapetelko <98822243+liapetelko@users.noreply.github.com>
2022-02-03 23:10:22 +0200

    Add files via upload

---
+++ tryVu.py
@@ -0,0 +1,42 @@
+#from curses import COLOR_CYAN
+#code won't run with this^ line 
+from sqlite3 import Date
+import streamlit as st 
+import pandas as pd
+
+st.title("Vugraph Sets")
+st.header("SOLOWAY 2019")
+st.markdown("DRIJVER BRINK GOOD MATCHES")
+
+#sets = pd.DataFrame({
+  #'first column': ["D 2019 Soloway QF - Segment 1 of 4", 2, 3, 4],
+  #'second column': ["1/1/22", 20, 30, 40]
+#})
+
+st.subheader("15 BOARDS") 
+s1 = st.checkbox("D 2019 Soloway QF - Segment 1 of 4")
+s2 = st.checkbox("D 2019 Soloway QF - Segment 2 of 4")
+s3 = st.checkbox("D 2019 Soloway QF - Segment 3 of 4") 
+s4 = st.checkbox("D 2019 Soloway QF - Segment 4 of 4")
+s5 = st.checkbox("A 2019 Soloway SF - Segment 1 of 4")
+s6 = st.checkbox("A 2019 Soloway SF - Segment 2 of 4")
+s7 = st.checkbox("A 2019 Soloway SF - Segment 4 of 4")
+s8 = st.checkbox("2019 Soloway FINAL - Segment 1 of 4")
+s9 = st.checkbox("2019 Soloway FINAL - Segment 2 of 4")
+s10 = st.checkbox("2019 Soloway FINAL - Segment 4 of 4")
+
+if s1:
+  dates1 = st.date_input("Enter")
+if s2:
+  dates2 = st.date_input("")
+if s3:
+  dates3 = st.date_input("")
+if s4:
+  dates4 = st.date_input("")
+if s5:
+  dates5 = st.date_input("")
+
+st.write("hi")
+st.checkbox("")
+#st.selectbox("",["D 2019 Soloway QF - Segment 1 of 4","D 2019 Soloway QF - Segment 2 of 4"])
+#s11
